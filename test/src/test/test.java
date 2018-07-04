ca4da95a5c62bbddfe6bc10deaacb1a4e9a95ef4
ruiqing0706 <2944925501@qq.com>
2018-07-04 10:07:00 +0800

    test.java

---
+++ test/src/test/test.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> urlNonFilterList = Arrays.asList(
+				"/api/tenant/userLogin", "/api", "/index.json","/api/tenant/getJoinDeptInfo"
+				,"/getDbAndTable.json","/error"
+				,"/dataStorage/resourceListNoSession.json","/resourceListNoSession.json"
+				,"/dataMonitor/api/tenant/userLoginWithout","/api/tenant/userLoginWithout"
+				);
+		String uri = "/aaa/api";
+		uri = uri.substring(4);
+		System.out.println(uri);
+		if (urlNonFilterList.contains(uri)) {
+			System.out.println("不需要session");
+		}
+	}
+
+}
